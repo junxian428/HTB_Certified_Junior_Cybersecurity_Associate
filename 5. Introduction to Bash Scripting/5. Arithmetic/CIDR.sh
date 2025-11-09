b05d27bfd250cb6be8e8b87eeabbc0a1c0d32a36
junxian428 <holeyiho@gmail.com>
2025-11-09 16:57:39 +0800

    update

---
+++ 5. Introduction to Bash Scripting/5. Arithmetic/CIDR.sh
@@ -0,0 +1,22 @@
+<SNIP>
+	echo -e "\nPinging host(s):"
+	for host in $cidr_ips
+	do
+		stat=1
+		while [ $stat -eq 1 ]
+		do
+			ping -c 2 $host > /dev/null 2>&1
+			if [ $? -eq 0 ]
+			then
+				echo "$host is up."
+				((stat--))
+				((hosts_up++))
+				((hosts_total++))
+			else
+				echo "$host is down."
+				((stat--))
+				((hosts_total++))
+			fi
+		done
+	done
+<SNIP>
